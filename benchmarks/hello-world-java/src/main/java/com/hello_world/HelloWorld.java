44f28bc1cc0894ffe135d891beff1a1a2e1c1935
jovanstevanovic <jovan.stevanovic1@outlook.com>
2021-03-25 15:31:54 +0100

    Added new benchmark. Renaming previously uploaded benchmark. Changed policy of connection between manager and vmm

---
+++ benchmarks/hello-world-java/src/main/java/com/hello_world/HelloWorld.java
@@ -0,0 +1,30 @@
+package com.hello_world;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class HelloWorld {
+
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(Integer.parseInt(args[0])), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(null); // creates a default executor
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            String response = "Hello world!\n";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
+}
