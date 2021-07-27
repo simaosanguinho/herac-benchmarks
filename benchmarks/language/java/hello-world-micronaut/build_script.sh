04d3c45d8a4063c539e13d0ee1680210b6b7f072
Jovan Stevanovic <44313413+jovanstevanovic@users.noreply.github.com>
2021-07-27 23:48:53 +0200

    Support for run tool (#64)

---
+++ benchmarks/language/java/hello-world-micronaut/build_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+cd "$DIR" || {
+  echo "Redirection failed!"
+  exit 1
+}
+./gradlew clean assemble
+echo BENCHMARK_PATH="$DIR"/build/libs/hello-world-micronaut-1.0-all.jar
