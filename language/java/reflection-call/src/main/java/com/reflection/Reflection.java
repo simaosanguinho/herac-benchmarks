4a6797c9901405b71b86775ef19b939dd3cf280f
Serhii Ivanenko <serhii.ivanenko@oracle.com>
2021-09-03 23:33:30 +0300

    Removed println that prints VMM boot time (#1)
    Removed remaining println in reflection-call benchmark

--- language/java/reflection-call/src/main/java/com/reflection/Reflection.java
+++ language/java/reflection-call/src/main/java/com/reflection/Reflection.java
@@ -11,7 +11,6 @@ import com.sun.net.httpserver.HttpServer;
 public class Reflection {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("VMM boot time: " + (System.currentTimeMillis() - Long.parseLong(args[args.length - 1])));
         HttpServer server = HttpServer.create(new InetSocketAddress(Integer.parseInt(args[0])), 0);
         server.createContext("/", new MyHandler());
         server.setExecutor(null); // creates a default executor
