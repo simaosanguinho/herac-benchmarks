0ae3d05d13ae4c673ad9787af62b4e628fb952e8
Rodrigo Bruno <rbruno@gsd.inesc-id.pt>
2022-08-06 14:20:51 +0000

    Adding sleep so benchmark.

---
+++ language/java/gv-sleep/src/main/java/com/sleep/Sleep.java
@@ -0,0 +1,36 @@
+package com.sleep;
+
+import java.lang.InterruptedException;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@SuppressWarnings("unused")
+public class Sleep {
+
+    static Map<String, Object> output = new HashMap<>();
+
+    static byte[] buffer;
+
+    public static void memory(int bytes) {
+        buffer = new byte[bytes];
+        for (int i = 0; i < bytes; i++) {
+            buffer[i] = 1;
+        }
+    }
+
+    public static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException ie) {
+            output.put("Log", "InterruptedException");
+        }
+    }
+
+    public static Map<String, Object> main(Map<String, Object> input) {
+        memory(Integer.parseInt((String)input.get("memory")));
+        sleep(Long.parseLong((String)input.get("sleep")));
+        return output;
+    }
+
+}
