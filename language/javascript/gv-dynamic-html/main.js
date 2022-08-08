cbbe94fb8a6154f6091d108754385356d2541183
Rodrigo Bruno <rbruno@gsd.inesc-id.pt>
2022-08-08 11:55:53 +0100

    Adding javascript dynamic html.

---
+++ language/javascript/gv-dynamic-html/main.js
@@ -0,0 +1,18 @@
+function random(b, e) {
+    return Math.round(Math.random() * (e - b) + b);
+}
+
+const main = async function (args) {
+    const output = new Map();
+    const args_split = args.split(";");
+    let url = args_split[0];
+    let username = args_split[1];
+    let nsize = args_split[2];
+    var random_numbers = new Array(nsize);
+    for(var i = 0; i < nsize; ++i) {
+        random_numbers[i] = random(0, 100);
+    }
+    let template_args = "{ \"cur_time\": \"" + new Date().toLocaleString() + "\", \"username\" : \"" + username  + "\", \"random_numbers\": [" + random_numbers + "]}";
+    output.set("result", jsHostAccess.mustache(String.fromCharCode(...jsHostAccess.downloadBytes(url)), template_args));
+    return output;
+}
