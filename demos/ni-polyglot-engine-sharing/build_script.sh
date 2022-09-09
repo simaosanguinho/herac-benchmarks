690c8dc859bd48c6c9f1398d5d2cfd418df05310
Rodrigo Bruno <rbruno@gsd.inesc-id.pt>
2022-09-09 14:05:10 +0100

    Minor improvements to engine sharing demo.

--- demos/ni-polyglot-engine-sharing/build_script.sh
+++ demos/ni-polyglot-engine-sharing/build_script.sh
@@ -10,6 +10,7 @@ function build_app {
 	cd build
 	$JAVA_HOME/bin/native-image \
 		--language:python \
+		--language:js \
 		-cp libs/demo-polyglot-engine-sharing-1.0-all.jar \
 		-H:Name=demopolyglot \
                 com.demo_polyglot.DemoPolyglot
@@ -17,6 +18,6 @@ function build_app {
 
 ./gradlew clean shadowJar assemble
 
-$JAVA_HOME/bin/java -cp build/libs/demo-polyglot-engine-sharing-1.0-all.jar com.demo_polyglot.DemoPolyglot ../../src/python/gv-thumbnail/main.py
+$JAVA_HOME/bin/java -cp build/libs/demo-polyglot-engine-sharing-1.0-all.jar com.demo_polyglot.DemoPolyglot python ../../src/python/gv-thumbnail/main.py http://localhost:8000/snap.png
 
 #build_app
--- demos/ni-polyglot-engine-sharing/src/main/java/com/demo_polyglot/DemoPolyglot.java
+++ demos/ni-polyglot-engine-sharing/src/main/java/com/demo_polyglot/DemoPolyglot.java
@@ -9,57 +9,55 @@ import org.graalvm.polyglot.Source;
 import org.graalvm.polyglot.Value;
 
 public class DemoPolyglot {
- 
-	private static long getMemoryFootprint_MBs() {
-		return (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (1024 * 1024);
-	}
-	
-    public static void main(String[] args) {
-    	String javaHome = System.getenv("JAVA_HOME");
-    	Map<String, String> options = new HashMap<>();
-    	long time1, time2, time3;
 
-    	if (args.length == 0) {
-    		System.err.println("Syntax: DemoPolyglot <path to python source file>");
-            System.exit(1);	
-    	}
-    	
+    private static long getMemoryFootprint_MBs() {
+        return (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (1024 * 1024);
+    }
+
+    private static void runContext(Engine engine, Source source, Map<String, String> options, String arguments) {
+        try (Context context = Context.newBuilder().allowAllAccess(true).options(options).engine(engine).build()) {
+            context.eval(source);
+            Value function = context.eval(source.getLanguage(), "main");
+            System.out.println(function.execute(arguments).toString());
+        }
+    }
+
+    public static void main(String[] args) {
+        String javaHome = System.getenv("JAVA_HOME");
+        Map<String, String> options = new HashMap<>();
+        Engine engine = Engine.create();
+        long time1, time2, time3;
+
+        if (args.length != 3) {
+            System.err.println("Syntax:  DemoPolyglot <python|js> <path to source file> <arguments>");
+            System.err.println("Example: DemoPolyglot python thumbnail.py http://localhost:8000/snap.png");
+            System.exit(1);
+        }
+
         if (javaHome == null) {
             System.err.println("JAVA_HOME not found in the environment. Polyglot functionality significantly limited.");
             System.exit(1);
         }
-        
-        // Path to languages.
+
+        Source source = Source.newBuilder(args[0], new File(args[1])).buildLiteral();
+
+        // Setting up the environment
         System.setProperty("org.graalvm.language.python.home", javaHome + "/languages/python");
         System.setProperty("org.graalvm.language.llvm.home", javaHome + "/languages/llvm");
         System.setProperty("org.graalvm.language.js.home", javaHome + "/languages/js");
-
-        // Python options.
-        options.put("python.ForceImportSite", "true");
-        options.put("python.Executable", javaHome + "/graalvisor-python-venv/bin/python");
-    	
-        System.out.println(String.format("Before Engine memory (MBs) = %s", getMemoryFootprint_MBs()));
-        try (Engine engine = Engine.create()) {
-            Source source = Source.newBuilder("python", new File(args[0])).buildLiteral();
-            System.out.println(String.format("Before Context1 memory (MBs) = %s", getMemoryFootprint_MBs()));
-            time1 = System.currentTimeMillis();
-            
-            try (Context context = Context.newBuilder().allowAllAccess(true).options(options).engine(engine).build()) {
-                context.eval(source);
-                Value function = context.eval("python", "main");
-                System.out.println(function.execute("http://localhost:8000/snap.png").toString());
-            }
-            System.out.println(String.format("Before Engine2 memory (MBs) = %s", getMemoryFootprint_MBs()));
-            time2 = System.currentTimeMillis();
-
-            try (Context context = Context.newBuilder().allowAllAccess(true).options(options).engine(engine).build()) {
-                context.eval(source);
-                Value function = context.eval("python", "main");
-                System.out.println(function.execute("http://localhost:8000/snap.png").toString());
-            }
-            System.out.println(String.format("Finish (MBs) = %s", getMemoryFootprint_MBs()));
-            time3 = System.currentTimeMillis();
+        if (source.getLanguage().equals("python")) {
+            options.put("python.ForceImportSite", "true");
+            options.put("python.Executable", javaHome + "/graalvisor-python-venv/bin/python");
         }
+
+        System.out.println(String.format("Before Context1 memory (MBs) = %s", getMemoryFootprint_MBs()));
+        time1 = System.currentTimeMillis();
+        runContext(engine, source, options, args[2]);
+        System.out.println(String.format("Before Engine2 memory (MBs) = %s", getMemoryFootprint_MBs()));
+        time2 = System.currentTimeMillis();
+        runContext(engine, source, options, args[2]);
+        System.out.println(String.format("Finish (MBs) = %s", getMemoryFootprint_MBs()));
+        time3 = System.currentTimeMillis();
         System.out.println(String.format("Context 1 took = %s, Context 2 took = %s", (time2 - time1), (time3 - time2)));
     }
 }
