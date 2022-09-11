4fdde72c71cf8870d3c9836afc20f1aae9a80ccd
SergiyIvan <serhii.ivanenko@oracle.com>
2022-09-12 00:06:58 +0100

    Adding memory allocation to the java/cr-sleep benchmark.

--- src/java/cr-sleep/Main.java
+++ src/java/cr-sleep/Main.java
@@ -1,15 +1,33 @@
 import com.google.gson.JsonObject;
 import java.lang.InterruptedException;
+import com.google.gson.JsonPrimitive;
 
 public class Main {
-    public static JsonObject main(JsonObject args) {
-        JsonObject response = new JsonObject();
+
+    static byte[] buffer;
+
+    public static void memory(int bytes) {
+        buffer = new byte[bytes];
+        for (int i = 0; i < bytes; i++) {
+            buffer[i] = 1;
+        }
+    }
+
+    public static void sleep(long millis, JsonObject response) {
         try {
-            long millis = args.getAsJsonPrimitive("sleep").getAsLong();
             Thread.sleep(millis);
         } catch (InterruptedException ie) {
             response.addProperty("output", "InterruptedException");
         }
+    }
+
+    public static JsonObject main(JsonObject args) {
+        JsonObject response = new JsonObject();
+        JsonPrimitive memoryObject = args.getAsJsonPrimitive("memory");
+        if (memoryObject != null) {
+            memory(memoryObject.getAsInt());
+        }
+        sleep(args.getAsJsonPrimitive("sleep").getAsLong(), response);
         return response;
     }
 }
