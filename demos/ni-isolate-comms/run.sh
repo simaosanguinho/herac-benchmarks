5b67bf118807c667105b9b708135c03b49566990
ivan-ristovic <ivan.ristovic@oracle.com>
2022-10-15 11:04:30 +0200

    Run comms example on HotSpot and SVM

--- demos/ni-isolate-comms/run.sh
+++ demos/ni-isolate-comms/run.sh
@@ -7,17 +7,41 @@ then
 fi
 
 cd build
+timestamp=$(date +%s)
+store_dir=../logs/$timestamp
+mkdir -p $store_dir
+
+echo $JAVA_HOME
 
 set -e;
 
-./demoisolatecomms --server $@ &
-serverpid=$!
-echo "server started, pid: $serverpid"
+
+echo "Running on HotSpot"
+
+$JAVA_HOME/bin/java \
+	-jar libs/demo-ni-isolate-comms-1.0-all.jar \
+	--server $@ \
+	| tee $store_dir/"$timestamp"_hotspot_server.log &
 
 sleep 1
 
-./demoisolatecomms --client $@ &
-clientpid=$!
-echo "client started, pid: $clientpid"
+$JAVA_HOME/bin/java \
+	-jar libs/demo-ni-isolate-comms-1.0-all.jar \
+	--client $@ \
+	| tee $store_dir/"$timestamp"_hotspot_client.log &
+
+wait
+
+echo "Running on SVM"
+
+./demoisolatecomms \
+	--server $@ \
+	| tee $store_dir/"$timestamp"_svm_server.log &
+
+sleep 1
+
+./demoisolatecomms \
+	--client $@ \
+	| tee $store_dir/"$timestamp"_svm_client.log &
 
 wait
