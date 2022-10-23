9aa83d0beba16ecf585f64c3e622bcbe42b150b6
ivan-ristovic <ivan.ristovic@oracle.com>
2022-10-23 20:46:01 +0200

    Move isolate comms bench to a separate PR

--- demos/ni-isolate-comms/src/main/java/com/demo_ni_isolate_comms/DemoLog.java
+++
@@ -1,9 +0,0 @@
-package com.demo_ni_isolate_comms;
-
-public class DemoLog {
-
-    public static void log(String msg) {
-        System.out.printf("[%d]: %s\n", System.nanoTime(), msg);
-    }
-    
-}
