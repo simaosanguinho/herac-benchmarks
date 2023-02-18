0f2fa96fc906b0f724909c80ff30b043c423a315
Rodrigo Bruno <rbruno@gsd.inesc-id.pt>
2023-02-18 10:00:59 -0500

    Reducing sleep time to 100 microseconds.

--- demos/ni-sharedmemory/src/main/java/com/demo/sharedmemory/SharedMemoryChannel.java
+++ demos/ni-sharedmemory/src/main/java/com/demo/sharedmemory/SharedMemoryChannel.java
@@ -20,9 +20,8 @@ public class SharedMemoryChannel {
                 (new File(path)).toPath(),
                 StandardOpenOption.READ,
                 StandardOpenOption.WRITE,
-                StandardOpenOption.CREATE );
+                StandardOpenOption.CREATE);
         buffer = channel.map(MapMode.READ_WRITE, 0, 4096).asCharBuffer();
-
     }
 
     public void initializeForWriting() {
@@ -32,7 +31,7 @@ public class SharedMemoryChannel {
     public void writeString(String s) throws InterruptedException {
         buffer.clear();
         while(buffer.get(0) != readyToWrite) {
-            Thread.sleep(1);
+            Thread.sleep(0, 100);
         }
         buffer.put(readyToWrite); // Advance the first char.
 
@@ -44,7 +43,7 @@ public class SharedMemoryChannel {
     public String readString() throws InterruptedException {
         buffer.clear();
         while(buffer.get(0) != readyToRead) {
-            Thread.sleep(1);
+            Thread.sleep(0, 100);
         }
         buffer.get(); // Advance the first char.
 
