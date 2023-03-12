bce5b8877972b15bfe3ac7eae14b3f82aa563662
Rodrigo Bruno <rbruno@gsd.inesc-id.pt>
2023-03-12 09:07:46 -0400

    Fixing text bug.

--- demos/firecracker/config-vm.sh
+++ demos/firecracker/config-vm.sh
@@ -6,7 +6,7 @@
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
 if [ -z "$1" ]; then
-    echo "Please a free IP 172.17.0.[2,255]. The IP will be used to create a firecracker directory and tap."
+    echo "Please use a free IP 172.17.0.[2,255]. The IP will be used to create a firecracker directory and tap."
     exit 0
 fi
 
