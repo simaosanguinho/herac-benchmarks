f84f6b8b992fa8059d4049612262455d512db01c
Rodrigo Bruno <rbruno@gsd.inesc-id.pt>
2023-03-29 23:08:10 -0400

    Added photons to plots.

--- plots/backups-plots/sandboxes-efficiency/plots/sandboxes-memory-norm.py
+++ plots/backups-plots/sandboxes-efficiency/plots/sandboxes-memory-norm.py
@@ -9,6 +9,7 @@ benchmark_labels = declarations.benchmark_labels
 isolate_benchmark_path = declarations.isolate_benchmark_path 
 process_benchmark_path = declarations.process_benchmark_path 
 openwhisk_benchmark_path = declarations.openwhisk_benchmark_path 
+photons_benchmark_path = declarations.photons_benchmark_path
 
 # Memory footprint in GBs.
 isolate_mem_avg = {}
@@ -17,6 +18,9 @@ process_mem_avg = {}
 process_mem_std = {}
 openwhisk_mem_avg = {}
 openwhisk_mem_std = {}
+photons_mem_avg = {}
+photons_mem_std = {}
+
 
 # Processing results.
 for idx, path in enumerate(isolate_benchmark_path):
@@ -28,6 +32,9 @@ for idx, path in enumerate(process_benchmark_path):
 for idx, path in enumerate(openwhisk_benchmark_path):
     openwhisk_mem_avg[idx] = results.process_result(path)["mem_avg"]
     openwhisk_mem_std[idx] = results.process_result(path)["mem_std"]
+for idx, path in enumerate(photons_benchmark_path):
+    photons_mem_avg[idx] = results.process_result(path)["mem_avg"]
+    photons_mem_std[idx] = results.process_result(path)["mem_std"]
 
 # Scale openwhisk to 2GB.
 for idx, path in enumerate(openwhisk_benchmark_path):
@@ -40,9 +47,17 @@ for idx, path in enumerate(process_benchmark_path):
 for idx, path in enumerate(openwhisk_benchmark_path):
         openwhisk_mem_avg[idx] = openwhisk_mem_avg[idx] / isolate_mem_avg[idx]
         openwhisk_mem_std[idx] = openwhisk_mem_std[idx] / isolate_mem_avg[idx]
+for idx, path in enumerate(photons_benchmark_path):
+    photons_mem_avg[idx] = photons_mem_avg[idx] / isolate_mem_avg[idx]
+    photons_mem_std[idx] = photons_mem_std[idx] / isolate_mem_avg[idx]
 for idx, path in enumerate(isolate_benchmark_path):
     isolate_mem_std[idx] = isolate_mem_std[idx] / isolate_mem_avg[idx]
     isolate_mem_avg[idx] = 1
+    # Filling Photons values with zeroes for missing (java) benchmarks.
+    if idx not in photons_mem_avg:
+        photons_mem_avg[idx] = 0
+        photons_mem_std[idx] = 0
+
 
 x = np.arange(len(benchmark_labels))
 width = 0.15
@@ -50,21 +65,20 @@ width = 0.15
 plt.rcParams.update({'font.size': 16})
 
 fig, ax = plt.subplots()
-ax.bar(x - width/3, isolate_mem_avg.values(),   yerr=isolate_mem_std.values(),   width=width, hatch='//', label='Graalvisor', alpha=0.75)
-ax.bar(x +   width, openwhisk_mem_avg.values(), yerr=openwhisk_mem_std.values(), width=width, hatch='-',  label='OpenWhisk',  alpha=0.75)
-ax.bar(x + width/3, process_mem_avg.values(),   yerr=process_mem_std.values(),   width=width, hatch='.',  label='SAND/SOCK',  alpha=0.75)
+ax.bar(x + 1*width, isolate_mem_avg.values(),   yerr=isolate_mem_std.values(),   width=width, hatch='//', label='Graalvisor', alpha=0.75)
+ax.bar(x + 2*width, process_mem_avg.values(),   yerr=process_mem_std.values(),   width=width, hatch='.',  label='SAND/SOCK',  alpha=0.75)
+ax.bar(x + 3*width, photons_mem_avg.values(),   yerr=photons_mem_std.values(),   width=width, hatch=',',  label='Photons',    alpha=0.75)
+ax.bar(x + 4*width, openwhisk_mem_avg.values(), yerr=openwhisk_mem_std.values(), width=width, hatch='-',  label='OpenWhisk',  alpha=0.75)
 
 ax.set_ylabel('Memory norm. to Isolate')
 ax.set_xticks(x, benchmark_labels)
-ax.legend(ncol=3)
 ax.set_axisbelow(True)
 plt.grid(axis = 'y', linestyle = '--', linewidth = 0.25)
 plt.xticks(rotation = 35)
-fig.set_figwidth(10)
+fig.set_figwidth(15)
 fig.set_figheight(4)
 
-ax.legend(ncol=3, bbox_to_anchor=(.85, 1.2))
+ax.legend(ncol=4)
 plt.savefig("sandboxes-memory-norm.pdf", bbox_inches='tight')
 plt.savefig("sandboxes-memory-norm.png", bbox_inches='tight')
-ax.legend(ncol=3)
 plt.show()
--- plots/backups-plots/sandboxes-efficiency/plots/sandboxes-tput-norm.py
+++ plots/backups-plots/sandboxes-efficiency/plots/sandboxes-tput-norm.py
@@ -9,6 +9,7 @@ benchmark_labels = declarations.benchmark_labels
 isolate_benchmark_path = declarations.isolate_benchmark_path 
 process_benchmark_path = declarations.process_benchmark_path 
 openwhisk_benchmark_path = declarations.openwhisk_benchmark_path 
+photons_benchmark_path = declarations.photons_benchmark_path
 
 # Throughput in ops/s.
 isolate_tput_avg = {}
@@ -17,6 +18,9 @@ process_tput_avg = {}
 process_tput_std = {}
 openwhisk_tput_avg = {}
 openwhisk_tput_std = {}
+photons_tput_avg = {}
+photons_tput_std = {}
+
 
 # Processing results.
 for idx, path in enumerate(isolate_benchmark_path):
@@ -28,6 +32,9 @@ for idx, path in enumerate(process_benchmark_path):
 for idx, path in enumerate(openwhisk_benchmark_path):
     openwhisk_tput_avg[idx] = results.process_result(path)["tput_avg"]
     openwhisk_tput_std[idx] = results.process_result(path)["tput_std"]
+for idx, path in enumerate(photons_benchmark_path):
+    photons_tput_avg[idx] = results.process_result(path)["tput_avg"]
+    photons_tput_std[idx] = results.process_result(path)["tput_std"]
 
 # Scale openwhisk to 2GB.
 for idx, path in enumerate(openwhisk_benchmark_path):
@@ -40,9 +47,17 @@ for idx, path in enumerate(process_benchmark_path):
 for idx, path in enumerate(openwhisk_benchmark_path):
         openwhisk_tput_avg[idx] = openwhisk_tput_avg[idx] / isolate_tput_avg[idx]
         openwhisk_tput_std[idx] = openwhisk_tput_std[idx] / isolate_tput_avg[idx]
+for idx, path in enumerate(photons_benchmark_path):
+    photons_tput_avg[idx] = photons_tput_avg[idx] / isolate_tput_avg[idx]
+    photons_tput_std[idx] = photons_tput_std[idx] / isolate_tput_avg[idx]
 for idx, path in enumerate(isolate_benchmark_path):
     isolate_tput_std[idx] = isolate_tput_std[idx] / isolate_tput_avg[idx]
     isolate_tput_avg[idx] = 1
+    # Filling Photons values with zeroes for missing (java) benchmarks.
+    if idx not in photons_tput_avg:
+        photons_tput_avg[idx] = 0
+        photons_tput_std[idx] = 0
+
 
 x = np.arange(len(benchmark_labels))
 width = 0.15
@@ -50,22 +65,21 @@ width = 0.15
 plt.rcParams.update({'font.size': 16})
 
 fig, ax = plt.subplots()
-ax.bar(x - width/3, isolate_tput_avg.values(), yerr=isolate_tput_std.values(), width=width, hatch='//', label='Graalvisor', alpha=0.75)
-ax.bar(x - width/3, openwhisk_tput_avg.values(), yerr=openwhisk_tput_std.values(), width=width, hatch='-', label='OpenWhisk', alpha=0.75)
-ax.bar(x + width/3, process_tput_avg.values(), yerr=process_tput_std.values(), width=width, hatch='.',  label='SAND/SOCK', alpha=0.75)
+ax.bar(x + 1*width, isolate_tput_avg.values(),   yerr=isolate_tput_std.values(),   width=width, hatch='//', label='Graalvisor', alpha=0.75)
+ax.bar(x + 2*width, process_tput_avg.values(),   yerr=process_tput_std.values(),   width=width, hatch='.',  label='SAND/SOCK',  alpha=0.75)
+ax.bar(x + 3*width, photons_tput_avg.values(),   yerr=photons_tput_std.values(),   width=width, hatch=',',  label='Photons',    alpha=0.75)
+ax.bar(x + 4*width, openwhisk_tput_avg.values(), yerr=openwhisk_tput_std.values(), width=width, hatch='-', label='OpenWhisk',   alpha=0.75)
 
 ax.set_ylabel('Tput norm. to Isolate')
 ax.set_xticks(x, benchmark_labels)
-ax.legend(ncol=3)
 ax.set_axisbelow(True)
 plt.grid(axis = 'y', linestyle = '--', linewidth = 0.25)
 plt.xticks(rotation = 35)
-fig.set_figwidth(10)
+fig.set_figwidth(15)
 fig.set_figheight(4)
 
-ax.legend(ncol=3, bbox_to_anchor=(.15, 1))
+ax.legend(ncol=4)
 plt.savefig("sandboxes-tput-norm.pdf", bbox_inches='tight')
 plt.savefig("sandboxes-tput-norm.png", bbox_inches='tight')
-ax.legend(ncol=3)
 plt.show()
 plt.show()
