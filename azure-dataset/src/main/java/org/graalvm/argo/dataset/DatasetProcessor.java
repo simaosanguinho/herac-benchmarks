4d69a91e02cd8d5cef1ccbda09af6512776b2391
Rodrigo Bruno <rbruno@gsd.inesc-id.pt>
2023-04-23 22:37:56 -0400

    Adding a new way to reduce the dataset, by concurrent invocations.

--- azure-dataset/src/main/java/org/graalvm/argo/dataset/DatasetProcessor.java
+++ azure-dataset/src/main/java/org/graalvm/argo/dataset/DatasetProcessor.java
@@ -22,107 +22,57 @@ import java.util.stream.Collectors;
 /**
  * This is the entry point for the Azure dataset processing
  * pipeline. Provide as input:
- * 1) The day of observations, that will be used as dataset ID (the dXX part)
- * 2) The first minute to consider
- * 3) The last minute to consider
- * 4) Desired max memory consumption during the experiment (in MB)
+ * 1) The day of observations, that will be used as dataset ID (the dXX part);
+ * 2) The first minute to consider;
+ * 3) The last minute to consider;
+ * 4) Desired max memory consumption during the experiment (in MB);
+ * 5) The maximum number of users that should be included in the resulting invocation set;
+ * 6) The maximum number of concurrent invocations;
  * This program expects raw dataset files to be placed in the "input" directory,
  * and will put results in the "output" directory.
  * Example usage:
- * $ java DatasetProcessor d01 1 2 1000
+ * $ java DatasetProcessor d01 1 2 1000 1000 1000
  * This way you will get all invocations for the first two minutes of
  * the day 1 from the initial dataset, so that the forecasted memory
- * consumption will not exceed 1 GB (1000 MB)
+ * consumption will not exceed 1 GB (1000 MB). We will also firter invocations
+ * from the most 1000 popular users and limit concurrent invocations to 1000.
  */
-// TODO - this explanation is good but is missing an explanation of what it produces.
-// Is this the class that produces invocations of each function with timestamps?
 public class DatasetProcessor {
 
     public static final String DELIMITER = ",";
     private static final int MINUTES_COLUMN_OFFSET = 3;
-    private static final int MOST_ACTIVE_USERS = 1000;
+    private static final List<Invocation> invocations = new LinkedList<>();
+    private static final Map<String, Owner> owners = new HashMap<>(2048);
+    private static int skipped = 0;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         String datasetId = args[0];
         int firstMinute = Integer.parseInt(args[1]);
         int lastMinute = Integer.parseInt(args[2]);
-        int maxMemory = Integer.parseInt(args[3]);
+        int maxMemory = args.length > 3 ? Integer.parseInt(args[3]) : 0;
+        int maxUsers = args.length > 4 ? Integer.parseInt(args[4]) : 0;
+        int maxConcInv = args.length > 5 ? Integer.parseInt(args[5]) : 0;
+
         FunctionInfoStorage.fillFunctionData(datasetId);
-        process(datasetId, firstMinute, lastMinute, maxMemory);
-    }
+        processDay(datasetId, firstMinute, lastMinute);
 
-    /*
-    HashOwner, HashApp, HashFunction, Trigger, 1, 2, 3...
-     */
-    private static void process(String datasetId, int firstMinute, int lastMinute, int maxMemory) {
-        List<Invocation> invocations = new LinkedList<>();
-        Map<String, Owner> owners = new HashMap<>(2048);
-
-        /* Read data from the CSV file, generate timestamps for the desired time frame */
-        try {
-            File file = new File("input/invocations_per_function_md.anon." + datasetId + ".csv");
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            String line;
-            String[] splitRow;
-            br.readLine(); // To skip the header
-            int skipped = 0;
-            while ((line = br.readLine()) != null) {
-                splitRow = line.split(DELIMITER);
-                String owner = splitRow[0];
-                String app = splitRow[1];
-                String function = splitRow[2];
-                /* If there is no record about this function about avg duration or memory, then skip */
-                if (!FunctionInfoStorage.DURATIONS.containsKey(function) || !FunctionInfoStorage.MEMORIES.containsKey(app)) {
-                    ++skipped;
-                    continue;
-                }
-
-                int memory = FunctionInfoStorage.MEMORIES.get(app);
-                int duration = FunctionInfoStorage.DURATIONS.get(function);
-
-                int currentMinute = firstMinute;
-                int invocationCount = 0;
-                while (currentMinute <= lastMinute) {
-                    int invocationsForMinute = Integer.parseInt(splitRow[currentMinute + MINUTES_COLUMN_OFFSET]);
-                    invocationCount += invocationsForMinute;
-                    int minBeginningMs = (currentMinute - 1) * 60000;
-                    int minEndMs = minBeginningMs + 60000;
-                    for (int i = 0; i < invocationsForMinute; ++i) {
-                        int timestamp = ThreadLocalRandom.current().nextInt(minBeginningMs, minEndMs);
-                        invocations.add(new Invocation(owner, function, memory, duration, timestamp));
-                    }
-                    ++currentMinute;
-                }
-                if (invocationCount > 0) {
-                    if (!owners.containsKey(owner)) {
-                        owners.put(owner, new Owner(owner));
-                    }
-                    Owner currentOwner = owners.get(owner);
-                    currentOwner.addFunction(function);
-                    currentOwner.addInvocations(invocationCount);
-                }
-            }
-            System.out.println("Skipped " + skipped + " functions due to lack of information.");
-            br.close();
-        } catch(IOException ioe) {
-            ioe.printStackTrace();
+        if (maxConcInv != 0) {
+            System.out.println("Number of invocations *before* filter by concurrent invocations: " + invocations.size());
+            downscaleByConcurrentInvocations(maxConcInv);
+            System.out.println("Number of invocations *after* filter by concurrent invocations: " + invocations.size());
         }
-        System.out.println("Number of owners in total: " + owners.size());
 
-        /* At this point, we have the unordered list of all invocations */
-        Collections.sort(invocations, Comparator.comparingInt(Invocation::getTimestamp));
-        System.out.println("Finished sorting.");
+        if (maxUsers != 0) {
+            System.out.println("Number of invocations *before* filter by number of users: " + invocations.size());
+            downscaleByUser(maxUsers);
+            System.out.println("Number of invocations *after* filter by number of users: " + invocations.size());
+        }
 
-        /* If needed, call printTraceSimulation on invocations here to get plot data for unfiltered trace */
-
-        /* get top X users based on number of functions */
-        Set<String> selectedOwners = owners.values().stream().sorted(Comparator.comparingInt(Owner::getFunctions).reversed())
-                .limit(MOST_ACTIVE_USERS).map(Owner::getOwnerHash).collect(Collectors.toSet());
-        System.out.println("Number of invocations before remove: " + invocations.size());
-        invocations.removeIf(i -> !selectedOwners.contains(i.getOwner()));
-        System.out.println("Number of invocations after remove: " + invocations.size());
-
-        downscaleByMemory(invocations, maxMemory);
+        if (maxMemory != 0) {
+            System.out.println("Number of invocations *before* filter by memory: " + invocations.size());
+            downscaleByMemory(maxMemory);
+            System.out.println("Number of invocations *after* filter by memory: " + invocations.size());
+        }
 
         /* Now we have ordered and downscaled list of all invocations that we can write as a result */
         List<String> acceptedInvocations = new LinkedList<>();
@@ -130,10 +80,100 @@ public class DatasetProcessor {
         acceptedInvocations.addAll(invocations.stream().map(Invocation::toString).collect(Collectors.toList()));
 
         Path resultFile = Paths.get("output", String.format("result_%s.csv", datasetId));
-        writeToFile(acceptedInvocations, resultFile);
+        Files.write(resultFile, acceptedInvocations, StandardCharsets.UTF_8);
     }
 
-    private static void downscaleByMemory(List<Invocation> invocations, int maxMemory) {
+    private static void processFunction(String line, int firstMinute, int lastMinute) {
+        String[] splitRow = line.split(DELIMITER);
+        String owner = splitRow[0];
+        String app = splitRow[1];
+        String function = splitRow[2];
+
+        /* If there is no record about this function about avg duration or memory, then skip */
+        if (!FunctionInfoStorage.DURATIONS.containsKey(function) || !FunctionInfoStorage.MEMORIES.containsKey(app)) {
+            ++skipped;
+            return;
+        }
+
+        int memory = FunctionInfoStorage.MEMORIES.get(app);
+        int duration = FunctionInfoStorage.DURATIONS.get(function);
+        int currentMinute = firstMinute;
+        int invocationCount = 0;
+        while (currentMinute <= lastMinute) {
+            int invocationsForMinute = Integer.parseInt(splitRow[currentMinute + MINUTES_COLUMN_OFFSET]);
+            invocationCount += invocationsForMinute;
+            int minBeginningMs = (currentMinute - 1) * 60000;
+            int minEndMs = minBeginningMs + 60000;
+            for (int i = 0; i < invocationsForMinute; ++i) {
+                int timestamp = ThreadLocalRandom.current().nextInt(minBeginningMs, minEndMs);
+                invocations.add(new Invocation(owner, function, memory, duration, timestamp));
+            }
+            ++currentMinute;
+        }
+        if (invocationCount > 0) {
+            if (!owners.containsKey(owner)) {
+                owners.put(owner, new Owner(owner));
+            }
+            Owner currentOwner = owners.get(owner);
+            currentOwner.addFunction(function);
+            currentOwner.addInvocations(invocationCount);
+        }
+    }
+
+    /*
+     * Read data from the CSV file, generate timestamps for the desired time frame.
+     * File expected syntax: HashOwner, HashApp, HashFunction, Trigger, 1, 2, 3...
+     */
+    private static void processDay(String datasetId, int firstMinute, int lastMinute) {
+        try {
+            File file = new File("input/invocations_per_function_md.anon." + datasetId + ".csv");
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            br.readLine(); // To skip the header
+
+            while ((line = br.readLine()) != null) {
+                processFunction(line, firstMinute, lastMinute);
+            }
+            System.out.println("Skipped " + skipped + " functions due to lack of information.");
+            br.close();
+        } catch(IOException ioe) {
+            ioe.printStackTrace();
+        }
+
+        /* At this point, we have the unordered list of all invocations */
+        Collections.sort(invocations, Comparator.comparingInt(Invocation::getTimestamp));
+        System.out.println("Finished sorting.");
+    }
+
+    /* Remove invocations that go over the maximum number of concurrent invocations. */
+    private static void downscaleByConcurrentInvocations(int maxConcInv) {
+        List<Invocation> activeInvocations = new LinkedList<>();
+        ListIterator<Invocation> iter = invocations.listIterator();
+        while (iter.hasNext()) {
+            Invocation currentInvocation = iter.next();
+            int currentInvocationTimestamp = currentInvocation.getTimestamp();
+
+            activeInvocations.removeIf(f -> currentInvocationTimestamp >= f.getEndTimestamp());
+            long currentInvokes = activeInvocations.stream().count();
+
+            if (currentInvokes + 1 <= maxConcInv) {
+                activeInvocations.add(currentInvocation);
+            } else {
+                iter.remove();
+            }
+        }
+    }
+
+    /* Remove invocations that are not from the N more popular users. */
+    private static void downscaleByUser(int maxUsers) {
+        Set<String> selectedOwners = owners.values().stream()
+                .sorted(Comparator.comparingInt(Owner::getFunctions).reversed())
+                .limit(maxUsers).map(Owner::getOwnerHash).collect(Collectors.toSet());
+        invocations.removeIf(i -> !selectedOwners.contains(i.getOwner()));
+    }
+
+    /* Remove invocations that go over the maximum memory. */
+    private static void downscaleByMemory(int maxMemory) {
         List<Invocation> activeInvocations = new LinkedList<>();
         ListIterator<Invocation> iter = invocations.listIterator();
         while (iter.hasNext()) {
@@ -148,14 +188,6 @@ public class DatasetProcessor {
             } else {
                 iter.remove();
             }
-        }
-    }
-
-    public static void writeToFile(List<String> data, Path file) {
-        try {
-            Files.write(file, data, StandardCharsets.UTF_8);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 }
--- azure-dataset/src/main/java/org/graalvm/argo/dataset/PlotDataGenerator.java
+++ azure-dataset/src/main/java/org/graalvm/argo/dataset/PlotDataGenerator.java
@@ -3,12 +3,20 @@ package org.graalvm.argo.dataset;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 // TODO - we need a better name :-)
 public class PlotDataGenerator {
-    public static void main(String[] args) {
+
+    private static final String AGGREGATED_INVOCATION_DATA = "%d %d %d %d %d %d %d %d";
+
+    public static void main(String[] args) throws Exception {
         String invocationsFile = args[0];
         String outputFile = args[1];
         int keepAlive = 0;
@@ -18,7 +26,7 @@ public class PlotDataGenerator {
         process(invocationsFile, outputFile, keepAlive);
     }
 
-    private static void process(String invocationsFile, String outputFile, int keepAlive) {
+    private static void process(String invocationsFile, String outputFile, int keepAlive) throws Exception {
         List<Invocation> invocations = new LinkedList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(invocationsFile))) {
             String line;
@@ -31,7 +39,41 @@ public class PlotDataGenerator {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        // TODO - Why do we need a different class? Isn't that class where we simulate the users and functions? Why is an util then?
-        PlottingUtils.printTraceSimulation(invocations, outputFile, keepAlive);
+        simulateTrace(invocations, outputFile, keepAlive);
+    }
+
+    private static void simulateTrace(List<Invocation> invocations, String filename, int keepAlive) throws Exception {
+        Set<Invocation> activeInvocations = new HashSet<>();
+        List<String> aggregatedInvocationData = new LinkedList<>();
+        System.out.println("Simulating trace with " + invocations.size() + " invocations");
+        int invocationsProcessed = 0;
+
+        for (Invocation currentInvocation : invocations) {
+            int currentInvocationTimestamp = currentInvocation.getTimestamp();
+            activeInvocations.removeIf(f -> currentInvocationTimestamp >= f.getEndTimestamp() + keepAlive);
+            activeInvocations.add(currentInvocation);
+            /* gather aggregated invocation data for plot */
+            long runningUsers = activeInvocations.parallelStream().filter(i -> i.getEndTimestamp() > currentInvocationTimestamp).map(Invocation::getOwner).distinct().count();
+            long runningFunctions  = activeInvocations.parallelStream().filter(i -> i.getEndTimestamp() > currentInvocationTimestamp).map(Invocation::getFunction).distinct().count();
+            long runningInvocations = activeInvocations.parallelStream().filter(i -> i.getEndTimestamp() > currentInvocationTimestamp).count();
+            long runningInvocationsFootprint = activeInvocations.parallelStream().filter(i -> i.getEndTimestamp() > currentInvocationTimestamp).mapToInt(Invocation::getMemory).sum();
+
+            long totalUsers = activeInvocations.parallelStream().map(Invocation::getOwner).distinct().count();
+            long totalFunctions = activeInvocations.parallelStream().map(Invocation::getFunction).distinct().count();
+
+            long cachedUsers = totalUsers - runningUsers;
+            long cachedFunctions = totalFunctions - runningFunctions;
+            // To calculate the size of our cache we are assuming that we can assume that we only keep one lambda live for each function.
+            // Then, we multiply the avg footprint of each function and we sum. The result is an estimate of our cache footprint.
+            long cachedInvocationsFootprint = cachedFunctions * 125; // TODO - we need to fix this. 125 is the avg of all functions...
+
+            // TODO - why not writing to disk directly? This will accumulate more and more memory.
+            aggregatedInvocationData.add(String.format(AGGREGATED_INVOCATION_DATA, currentInvocationTimestamp, runningUsers, runningFunctions, cachedUsers, cachedFunctions, runningInvocations, runningInvocationsFootprint, cachedInvocationsFootprint));
+            if (invocationsProcessed % 10000 == 0) {
+                System.out.println(invocationsProcessed);
+            }
+            ++invocationsProcessed;
+        }
+        Files.write(Paths.get("output", filename), aggregatedInvocationData, StandardCharsets.UTF_8);
     }
 }
