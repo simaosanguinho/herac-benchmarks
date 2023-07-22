ebf3ac4970835c5704df3e81ace5cd080337ac26
Rodrigo Bruno <rbruno@gsd.inesc-id.pt>
2023-07-22 09:55:51 +0100

    Improving dna benchmark.

---
+++ src/python/gv-dna/src/main/python/main-orig.py
@@ -0,0 +1,28 @@
+import datetime
+import os
+import requests
+from squiggle import transform
+
+def dna(fasta_url):
+    with open("/tmp/bacillus_subtilis.fasta", 'wb') as ofile:
+        response = requests.get(fasta_url)
+        ofile.write(response.content)
+
+    process_begin = datetime.datetime.now()
+    result = transform(open("/tmp/bacillus_subtilis.fasta", "r").read())
+    process_end = datetime.datetime.now()
+
+    return {
+            'result': (result[0][:10], result[1][:10]),
+            'measurement': {
+                'compute_time': (process_end - process_begin) / datetime.timedelta(microseconds=1)
+            }
+    }
+
+def main(fasta_url):
+    try:
+        return {"result": dna(fasta_url)}
+    except Exception as e:
+        return {"result": str(e)}
+
+#main("http://127.0.0.1:8000/bacillus_subtilis.fasta")
