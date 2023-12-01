939c5b38340bbcc13665db0ffe32878fa02971f7
SergiyIvan <serhii.ivanenko@oracle.com>
2023-12-01 21:28:53 +0000

    Minor fixes in the mw-test script.

--- azure-dataset/mw-test.sh
+++ azure-dataset/mw-test.sh
@@ -5,16 +5,27 @@ function DIR {
 AZURE_EXECUTOR_JAR=$(DIR)/build/libs/azure-dataset-1.0-all.jar
 AZURE_EXECUTOR_ENTRYPOINT=org.graalvm.argo.dataset.execution.ExecutorEntryPoint
 
-function_code="$ARGO_HOME/../benchmarks/src/java/gv-genericapp/build/libgenericapp.so"
-function_entry_point="com.genericapp.GenericApp"
+
+MODE=$1
+DATASET_FILE=$2
+
+if [[ "$MODE" = "gv" ]]; then
+    FUNCTION_ISOLATION=false
+    INVOCATION_COLLOCATION=true
+elif [[ "$MODE" = "gv-sf" ]]; then
+    FUNCTION_ISOLATION=true
+    INVOCATION_COLLOCATION=true
+elif [[ "$MODE" = "gv-si" ]]; then
+    FUNCTION_ISOLATION=true
+    INVOCATION_COLLOCATION=false
+else
+    echo "Syntax: <mode> </path/to/dataset/directory>"
+	exit 1
+fi
 
 time $JAVA_HOME/bin/java -cp $AZURE_EXECUTOR_JAR $AZURE_EXECUTOR_ENTRYPOINT \
-        --input $1 \
-        --functionCode $function_code \
-        --functionLanguage java \
-        --functionEntryPoint $function_entry_point \
-        --functionMemory 16 \
+        --input $DATASET_FILE \
         --functionRuntime "graalvisor" \
-        --invocationCollocation "true" \
-        --functionIsolation "false" \
-        --multiWorker
+        --invocationCollocation $INVOCATION_COLLOCATION \
+        --functionIsolation $FUNCTION_ISOLATION \
+        --multiWorker > /tmp/mw_test_execution.log
