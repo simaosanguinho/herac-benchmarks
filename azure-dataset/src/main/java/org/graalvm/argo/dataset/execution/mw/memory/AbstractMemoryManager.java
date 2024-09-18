ef6dc02daaea5b42bb62ac768be39106578c2007
Serhii Ivanenko <serhii.ivanenko@tecnico.ulisboa.pt>
2024-09-18 13:52:17 +0100

    Removing azure-dataset, fake-worker, and large-scale experiment-related scripts as they are moved to a different repository.

--- azure-dataset/src/main/java/org/graalvm/argo/dataset/execution/mw/memory/AbstractMemoryManager.java
+++
@@ -1,8 +0,0 @@
-package org.graalvm.argo.dataset.execution.mw.memory;
-
-public interface AbstractMemoryManager {
-    void startRequest(String ownerName, String functionName, int memory);
-    void finishRequest(String ownerName, String functionName);
-    boolean canAccommodateRequest(String ownerName, String functionName, int memory);
-    int getCurrentMemoryConsumption();
-}
